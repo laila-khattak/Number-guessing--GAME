1cd98346dd95508b9608504f5e1fd541583bab28
laila-khattak <166266458+laila-khattak@users.noreply.github.com>
2024-04-18 03:35:04 +0500

    Add files via upload

---
+++ index.ts
@@ -0,0 +1,21 @@
+#!/usr/bin/env/node
+
+import inquirer from "inquirer"
+   
+    const randomnumber = Math.floor(Math.random()* 10 +2)
+
+   
+
+   const answer = await inquirer.prompt([{
+
+        name:"UserguessedNumber",
+        type:"number",
+        message:"please Guessed the Number b/w 1 to 10"
+
+   }]);
+   
+   if(answer.UserguessedNumber === randomnumber){
+         console.log("congratulation you guessed right number")}
+
+ else{ console.log("sorry you guessed wrong number");
+ }     
